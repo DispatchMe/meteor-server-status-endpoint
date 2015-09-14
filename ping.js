b9f0e5500f2730596c0a31a61b161f0d6c08868a
Jordan Kelly <jordan@dispatch.me>
2015-09-14 16:50:03 -0700

    Remove defaults & cleanup settings
    Remove default interval & move interval out of public.
    Only ping if interval is set.
    Remove unneeded object protection.
    
    Minor: Cleanup formatting.

--- ping.js
+++ ping.js
@@ -1,10 +1,9 @@
 /* global CloudWatch:false - from dispatch:cloudwatch-metrics */
 
-var DEFAULT_PING_INTERVAL = 60000; // 1m
+var pingInterval = Meteor.settings.pingInterval;
 
-var settings = Meteor && Meteor.settings && Meteor.settings.public || {};
-var pingInterval = settings.pingInterval || DEFAULT_PING_INTERVAL;
-
-Meteor.setInterval(function () {
+if (pingInterval) {
+  Meteor.setInterval(function () {
     CloudWatch.track('ping', 1);
-}, pingInterval);
+  }, pingInterval);
+}
